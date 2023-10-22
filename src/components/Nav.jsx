7b0fa6c60211fd28a0590bc457a68ae13e3e22fd
black.0190@icloud.com <black.0190@icloud.com>
2023-10-22 20:05:46 +0900

    first commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+export default function Nav() {
+    return (
+        <navigator className="nav">
+            <div className="nav">
+                <nav className="nav ul">
+                    <ul>
+                        <li><span>Home</span></li>
+                        <li><a href="#">Get started</a></li>
+                        <li><a href="#">Photos</a></li>
+                        <li><a href="#">Gear</a></li>
+                        <li><a href="#">Forum</a></li>
+                    </ul>
+
+                </nav>
+            </div>
+        </navigator>
+    );
+}
