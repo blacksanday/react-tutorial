467e7743fe47dc32b570b8757b3ab400fb0e4eea
black.0190@icloud.com <black.0190@icloud.com>
2023-10-23 15:14:56 +0900

    ko

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import Header from './components/Header'
-import Nav from './components/nav'
+import Nav from './components/nav';
 import Main from './components/Main'
 import Footer from './components/Footer'
 export default function App() {
-    return (
-      <div>
-        <Header />
-        <Nav />
-        <Main />
-        <Footer />
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Header />
+      <Nav />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
